36a09a35a1eedf440c5cbc1e25394f308d158e98
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2018-03-28 19:02:26 +0300

    prod

--- contracts/SBIToken.sol
+++ contracts/SBIToken.sol
@@ -114,7 +114,7 @@ contract SBIToken is Owned, CrowdsaleParameters {
     using SafeMath for uint256;
     /* Public variables of the token */
     string public standard = 'ERC20/SBI';
-    string public name = 'Subsoil Blockchain investitions';
+    string public name = 'Subsoil Blockchain Investitions';
     string public symbol = 'SBI';
     uint8 public decimals = 18;
 
